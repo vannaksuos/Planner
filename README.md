0285783c8edce5fa6d7e5a75cbea85d8909ac9d1
Vannak <vannaksuos@gmail.com>
2020-02-25 12:32:12 -0500

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Planner
+this is a planner just for one day
+it has hours 8 hours for a working day
+it has a save icon to save text into local storage
+it also change color in the description box depending if its the past, present or future task.
+its has the date on using moment.js
---
+++ planner.js
@@ -0,0 +1,64 @@
+// this function will get all the key from local storage
+function getLocalStorage(key) {
+    let value = localStorage.getItem(key);
+    if (value) {
+        $(`#text${key}`).text(value);
+    }
+}
+
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    for (let i = 9; i < 18; i++) {
+        // creating a row
+        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+        // creating a column
+        var col1 = $('<div class="col-sm-2"> <p class="hour">' + pmAM(i) + '</p>');
+        //creating column 2
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="tasks" placeholder="Add event here..."></textarea>`);
+        //creating column 3
+        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+        // appending col to row
+        row.append(col1);
+        row.append(col2);
+        row.append(col3);
+        //  adding rows to container
+        $(".container").append(row);
+        getLocalStorage(i);
+    }
+
+    function pmAM(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
+    pmAM();
+
+    // this is the saveBTn event listener, when click it will save whats in this description text area
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function() {
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings().children('.tasks').val();
+        localStorage.setItem(eventId, eventText);
+    });
+});
+// create a function to update the colors by past present and future
+    function updateColors() {
+    var currentTime = new Date().getHours();
+    for (var i = 9; i < 18; i++) {
+        console.log(currentTime, $(`#${i}`).data("time"));
+        if ($(`#${i}`).data("time") == currentTime) {
+            $(`#text${i}`).addClass("present");
+        } else if (currentTime < $(`#${i}`).data("time")) {
+            $(`#text${i}`).addClass("future");
+        }
+    }
+}
+// setting a fucntion that will update the color depending of the time if its pass present or future and call the updateColors function
+setInterval(function() {
+    updateColors();
+}, 1000);
+
+
+
+
